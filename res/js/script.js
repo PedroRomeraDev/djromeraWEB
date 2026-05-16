e85d1ad9f2012e8144ba29ba0dc7d41665b99e65
PedroRomeraDev <promeraamado@gmail.com>
2026-05-16 23:13:05 +0200

    a

---
+++ res/js/script.js
@@ -0,0 +1,33 @@
+const radio = document.getElementById("radio");
+    const button = document.getElementById("playButton");
+    const icon = document.getElementById("icon");
+
+    let playing = false;
+
+    button.addEventListener("click", async () => {
+
+      if(!playing){
+
+        try{
+          await radio.play();
+
+          icon.classList.remove("play-icon");
+          icon.classList.add("pause-icon");
+
+          playing = true;
+
+        }catch(err){
+          console.log(err);
+        }
+
+      }else{
+
+        radio.pause();
+
+        icon.classList.remove("pause-icon");
+        icon.classList.add("play-icon");
+
+        playing = false;
+      }
+
+    });
